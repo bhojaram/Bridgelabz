5559e412439e49c6bd052c1c9d62b260584c0d07
moneshs <moneshsundar19@gmail.com>
2019-12-31 02:22:47 -0500

    [bhojaram].Added modify Flipcoin

--- Functional/Flipcoin.py
+++ Functional/Flipcoin.py
@@ -1,18 +1,9 @@
-import array as arr
-a = arr. array( 'f', [0.1, 0.2,0.6,0.7,0.4])
-#initilising count variables
-counthead=0
-counttail=0
-for i in range (len(a)):
-    
-    if(a[i]> 0.5):
-        
-        counthead = counthead+1
-       
-    else :
-        counttail = counttail+1
-#calculate percentage and printing        
-percentage = (counthead / (len(a)))*100
-print(percentage) 
-percentage = (counttail / (len(a)))*100
-print(percentage)    
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import Flipcoin
+
+#taking array inputs   
+a =arr. array( 'd', [0.1, 0.2,0.6,0.7,0.4])
+#a= [0.1, 0.2,0.6,0.7,0.4]
+
+Flipcoin(a)
