142ec61e367961c9e473e23b2dde8ab56b2c2706
moneshs <moneshsundar19@gmail.com>
2019-12-31 00:26:53 -0500

    [bhojaram].Added primefactors

---
+++ Functional/Primefactor.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import Primefactor
+
+#taking user input as integer
+#also calling function
+num =input("enter a number :" )
+Primefactor(num)
