badd0c1190d2ad34151900e3611f240674fc476b
moneshs <moneshsundar19@gmail.com>
2019-12-31 01:14:24 -0500

    [bhojaram].Added stopwatch time elapsed

---
+++ Functional/Stopwatch.py
@@ -0,0 +1,8 @@
+import sys
+import datetime
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import Stopwatch
+
+start_time =datetime.datetime.now()
+end_time   = datetime.datetime.now()
+Stopwatch()
