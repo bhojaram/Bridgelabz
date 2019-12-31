134a618ae34526de3b51351428fdc51161cc64a4
moneshs <moneshsundar19@gmail.com>
2019-12-31 06:18:53 -0500

    [bhojaram].Added coupon number problem

---
+++ Functional/Couponnumber.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import Couponnumber
+
+#taking distinct numbers as input
+a= arr.array( 'i' [3,4,7,8,9,5,1])
+Couponnumber(a)
