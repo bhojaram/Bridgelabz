3777a6ba0036ceb5d6f9e2bd9d48c432f2879c92
moneshs <moneshsundar19@gmail.com>
2020-01-08 00:44:10 -0500

    [bhojaram] added orderlist

---
+++ Datastructure/orderlist.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import linked_list
+#main method of linked_list       
+if __name__=='__main__':
+    l=linked_list()
+    l.printlist()
