38ea63a09e6763296e5c40c88c775af71c3c8dbc
moneshs <moneshsundar19@gmail.com>
2020-01-10 07:51:14 -0500

    [bhojaram] added palindrome using stack

---
+++ Datastructure/palindrome_checker.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import dqueue 
+#main method   
+if __name__== '__main__':
+    q = dqueue()
+    str1 = "abba"
+    length = q.d_size()
+    q.stringToListNode(str1)
+    self.                       
