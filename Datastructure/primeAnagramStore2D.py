6938da57fee4d85736d24f1e92e2349ae33145b1
moneshs <moneshsundar19@gmail.com>
2020-01-11 06:32:08 -0500

    [bhojaram] added store prime anagram

---
+++ Datastructure/primeAnagramStore2D.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import primeAnagramStore2D  
+#calling main method              
+if __name__=='__main__':
+    l=2
+    h=1000
+    primeAnagramStore2D(l,h)
