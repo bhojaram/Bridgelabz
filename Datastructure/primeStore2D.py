caffb9778475ff5305f792b3a8556234108d69c5
moneshs <moneshsundar19@gmail.com>
2020-01-11 06:34:29 -0500

    [bhojaram] added store prime in 2D array

---
+++ Datastructure/primeStore2D.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/home/user/Bridgelabz')
+from Utility.utility import primeStore2D
+#taking range & start of main
+if __name__=='__main__':
+l=2
+h=1000
+lst = []
+primeStore2D(l,h)
