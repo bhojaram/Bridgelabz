30856ad31191b5dda8baef410dce52d46444f694
bhojaram <bhojaramsa@gmail.com>
2020-01-13 08:04:12 -0500

    [bhojaram] modified palindrome program using Dqueue program

--- Datastructure/palindrome_checker.py
+++ Datastructure/palindrome_checker.py
@@ -7,4 +7,12 @@ if __name__== '__main__':
     str1 = "abba"
     length = q.d_size()
     q.stringToListNode(str1)
-    self.                       
+    while d.d_size() > 1 :
+        first = d.remove_head()
+        last = d.remove_tail()
+        if first != last:
+            print("not palindrome")
+        else :
+            continue
+    print("palindrome")  
+                          
