f5f7eef86a245b24d53d5be86be7437bc91e47eb
bhojaram <bhojaramsa@gmail.com>
2020-01-17 00:18:28 -0500

    [bhojaram] modified functional

--- Functional/Stopwatch.py
+++ Functional/Stopwatch.py
@@ -3,6 +3,6 @@ import datetime
 sys.path.append('/home/user/Bridgelabz')
 from Utility.utility import Stopwatch
 
-start_time = datetime.datetime.now()
-end_time   = datetime.datetime.now()
-Stopwatch()
+start_time = datetime.now()
+end_time   = datetime.now()
+Stopwatch(start_time,end_time)
